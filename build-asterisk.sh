8efe23f4d1fcaf0041ef894b1f5f6170dfd56e6e
Lucas Gois <lucas.gois@fluxoti.com>
2017-03-27 16:39:55 -0300

    Building Asterisk 14

--- build-asterisk.sh
+++ build-asterisk.sh
@@ -4,31 +4,50 @@ set -ex
 mkdir -p /usr/src/asterisk
 cd /usr/src/asterisk
 
-curl -vsL http://downloads.asterisk.org/pub/telephony/asterisk/releases/asterisk-${ASTERISK_VERSION}.tar.gz |
-    tar --strip-components 1 -xz
-
 # 1.5 jobs per core works out okay
 : ${JOBS:=$(( $(nproc) + $(nproc) / 2 ))}
 
-./configure  --with-resample --with-pjproject-bundled
+echo -e "\e[34m ---> Downloading Pjproject\e[0m"
+curl -svL http://www.pjsip.org/release/${PJPROJECT_VERSION}/pjproject-${PJPROJECT_VERSION}.tar.bz2 > pjproject-${PJPROJECT_VERSION}.tar.bz2
+
+echo -e "\e[34m ---> Building Pjproject\e[0m"
+tar -xjvf pjproject-${PJPROJECT_VERSION}.tar.bz2
+cd pjproject-${PJPROJECT_VERSION}
+./configure --prefix=/usr --enable-shared --disable-sound --disable-resample --disable-video --disable-opencore-amr CFLAGS='-O2 -DNDEBUG'
+make dep
+make
+make install
+ldconfig
+
+echo -e "\e[34m ---> Downloading Asterisk\e[0m"
+cd ..
+curl -sL http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-${ASTERISK_VERSION}.tar.gz |
+    tar --strip-components 1 -xz
+
+echo -e "\e[34m ---> Building Asterisk\e[0m"
+./bootstrap.sh
+./contrib/scripts/get_mp3_source.sh
+./contrib/scripts/install_prereq install
+./configure --with-resample
+
 make menuselect/menuselect menuselect-tree menuselect.makeopts
+menuselect/menuselect --disable BUILD_NATIVE \
+--enable BETTER_BACKTRACES  \
+--enable codec_opus \
+--enable func_odbc \
+--enable res_odbc \
+--enable res_pjsip \
+menuselect.makeopts
 
-# disable BUILD_NATIVE to avoid platform issues
-menuselect/menuselect --disable BUILD_NATIVE menuselect.makeopts
-
-# enable good things
-menuselect/menuselect --enable BETTER_BACKTRACES menuselect.makeopts
-menuselect/menuselect --enable codec_opus menuselect.makeopts
-menuselect/menuselect --enable func_odbc menuselect.makeopts
-menuselect/menuselect --enable res_odbc menuselect.makeopts
-
+echo -e "\e[34m ---> Downloading Core Sounds\e[0m"
 # download more sounds
-for i in CORE-SOUNDS-EN MOH-OPSOUND EXTRA-SOUNDS-EN; do
+for i in CORE-SOUNDS-EN; do
     for j in ULAW ALAW G722 GSM SLN16; do
         menuselect/menuselect --enable $i-$j menuselect.makeopts
     done
 done
 
+echo -e "\e[34m ---> Installing Asterisk\e[0m"
 make -j ${JOBS} all
 make install
 chown -R asterisk:asterisk /var/*/asterisk
